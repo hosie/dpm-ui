f64d847a997b3889763371318ac0bb652e763cb4
Hosie <jhosie@uk.ibm.com>
2019-11-05 05:34:12 +0000

    Display sample name and mode on pad

--- src/Pad.js
+++ src/Pad.js
@@ -2,10 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types'
 import './Pad.css';
 
-function Pad({onPadClick, isActive}) {
+function Pad({onPadClick, isActive,sample,mode}) {
   return (
     <div className={isActive? "pad--active" : "pad"}>
       <div onClick={onPadClick} className="pad--inner-border">
+        {
+          typeof sample !== 'undefined' &&
+          <div>
+            {mode?mode:'one-shot'}
+            {sample}
+          </div>
+        }
 
       </div>
     </div>
@@ -14,7 +21,9 @@ function Pad({onPadClick, isActive}) {
 
 Pad.propTypes = {
   onPadClick: PropTypes.func.isRequired,
-  isActive: PropTypes.bool.isRequired
+  isActive: PropTypes.bool.isRequired,
+  mode: PropTypes.string,
+  sample: PropTypes.string
 }
 
 export default Pad;
--- src/PadContainer.js
+++ src/PadContainer.js
@@ -2,8 +2,24 @@ import { connect } from 'react-redux'
 import Pad from './Pad.js'
 import { activatePad } from './actions'
 const mapStateToProps = (state,ownProps) => {
+  let padId = ownProps.padId
+  let mode
+  let sample
+  if(typeof state.pads[padId] !== 'undefined') {
+    mode = state.pads[padId].mode
+    let sampleId = state.pads[padId].sampleId
+    if (typeof sampleId !== 'undefined'){
+      if(typeof state.samples.files[sampleId] !== 'undefined') {
+        sample=state.samples.files[sampleId].filename
+      } else {
+        console.error(`undefined sampleId ${sampleId} for pad ${padId}`)
+      }
+    }
+  }
   return {
-    isActive: state.padBank.activePadId === ownProps.padId
+    isActive: state.padBank.activePadId === padId,
+    mode,
+    sample
   }
 }
 
